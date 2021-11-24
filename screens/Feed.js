0f1d388d94dcbcfbe6a367d3b24aeaf060ed0e7d
kshitij1127 <gudlakshitij.1127@gmail.com>
2021-11-24 18:08:11 +0530

    errors

--- screens/Feed.js
+++ screens/Feed.js
@@ -1,37 +1,78 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import Button from '@mui/material/Button';
-import axios from 'axios';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import Button from "@mui/material/Button";
+import axios from "axios";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
 
 export default class Feed extends React.Component {
-    getNews = () => {
-        const api = 'https://newsapi.org/v2/everything?q=Apple&from=2021-11-23&sortBy=popularity&apiKey=4482bf8b34b2410eaceaa7fe6f3c3a93'
-        axios.get(api).then(res => {
-            console.log(res.data.articles[0].title)
-        })
-    }
-  render() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      news: [],
+    };
+  }
+  getNews = () => {
+    const api =
+      "https://newsapi.org/v2/everything?q=everything&from=2021-11-23&sortBy=popularity&apiKey=4482bf8b34b2410eaceaa7fe6f3c3a93";
+    axios.get(api).then((res) => {
+      for (let i = 0; i < res.data.articles.length; i++) {
+        console.log(res.data.articles[i].title);
+        this.setState({
+          news: res.data.articles[i].title,
+        });
+      }
+    });
+  };
+
+  displayNews = () => {
     return (
       <View>
+        <Text>{this.state.news}</Text>
+      </View>
+    );
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
         <Button
           variant="text"
           onClick={() => {
-            this.props.navigation.navigate('Welcome');
-          }}>
+            this.props.navigation.navigate("Welcome");
+          }}
+        >
           Go Back
         </Button>
-        <Text>Feed</Text>
-        <View style={styles.container}>
-            <Card>
-                <CardContent>
-                    <Typography color="text.primary" sx={{fontSize: 56}}>Headlines</Typography>
-                    <Typography color="text.primary" sx={{ fontSize: 20 }}>{this.getNews()}</Typography>
-                </CardContent>
-            </Card>
+
+        <View>
+          <Card>
+            <CardContent>
+              <Typography color="text.primary" sx={{ fontSize: 56 }}>
+                Headlines
+              </Typography>
+              <Typography color="text.primary" sx={{ fontSize: 20 }}>
+                {this.getNews()}
+              </Typography>
+            </CardContent>
+          </Card>
         </View>
+
+        <View>
+          {this.state.news.map((news) => (
+          <Card>
+            <CardContent>
+              <Typography color="text.primary" sx={{ fontSize: 20, color: "black" }}>
+                {/* {this.displayNews()} */}
+                {news}
+                {console.log(news)}
+              </Typography>
+            </CardContent>
+          </Card>
+          ))}
+        </View>
+        
       </View>
     );
   }
@@ -39,8 +80,8 @@ export default class Feed extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    justifyContent: 'center', 
-    alignItems: 'center',
-    flex: 1,
-  }
-})
+    justifyContent: "center",
+    alignItems: "center",
+    flex: 5,
+  },
+});
